6a6d0107e9a1d226790f2e0b93b94e22b1b8a004
David-Rodden <davidrodden273@gmail.com>
2023-01-24 01:00:07 -0800

    altered template to include coordinate switch

--- app.py
+++ app.py
@@ -5,25 +5,31 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template("location.html")
 
 
-@app.route('/location')
-def location():
-    return render_template('location.html')
+# @app.route('/location')
+# def location():
+#     return render_template('location.html')
 
 
 @app.route('/location_data', methods=['POST'])
 def location_data():
     lat = request.form.get('lat')
     lon = request.form.get('lon')
-    geolocator = Nominatim(user_agent="myGeocoder")
-    location = geolocator.reverse(f"{lat}, {lon}", exactly_one=True)
-    city = location.raw['address'].get('town') or location.raw['address'].get('city') or location.raw['address'].get(
-        'village')
-    return jsonify({"city": city} if city else {"error": "Unable to determine location"})
+    return jsonify(lat=lat, lon=lon)
+    # geolocator = Nominatim(user_agent="myGeocoder")
+    # location = geolocator.reverse(f"{lat}, {lon}", exactly_one=True)
+    # city = location.raw['address'].get('town') or location.raw['address'].get('city') or location.raw['address'].get(
+    #     'village')
+    # return jsonify({"city": city} if city else {"error": "Unable to determine location"})
 
+@app.route('/get_coordinates')
+def get_coordinates():
+    lat = 45.5231
+    lon = -122.6765
+    return jsonify(lat=lat, lon=lon)
 
 if __name__ == '__main__':
     app.run()
